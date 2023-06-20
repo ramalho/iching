ecf008fda9e36483bb96681fc5a633bd0fb827d5
Luciano Ramalho <luciano@ramalho.org>
2023-06-20 16:39:08 -0300

    make notebook use gua.Yao

--- gua.py
+++ gua.py
@@ -1,4 +1,6 @@
-from enum import Enum
+from array import array
+from enum import IntEnum
+import random
 
 # U+2501  ━  BOX DRAWINGS HEAVY HORIZONTAL
 # U+2715  ✕  MULTIPLICATION X
@@ -18,15 +20,16 @@ ODDS = {
     9: 3,
 }
 
+
 assert sum(ODDS.values()) == 16, 'sum must be 16'
 
 outcome_groups = ([result] * chances for result, chances in ODDS.items())
-OUTCOMES = [o for group in outcome_groups for o in group]
+OUTCOMES = array('B', (o for group in outcome_groups for o in group))
+
 assert len(OUTCOMES) == 16, 'length of OUTCOMES must be 16'
 assert sum(o % 2 for o in OUTCOMES) == 8, 'count of odd outcomes must be 8'
 
-
-class Yao(Enum):
+class Yao(IntEnum):
     OLD_YIN = 6
     YANG = 7
     YIN = 8
@@ -34,4 +37,20 @@ class Yao(Enum):
 
     def draw(self):
         return LINE_DRAWINGS[self.value]
+
+    def mutate(self):
+        if self is Yao.OLD_YIN:
+            return Yao.YANG
+        elif self is Yao.OLD_YANG:
+            return Yao.YIN
+        return self
+
+    @classmethod
+    def pick(cls):
+        return cls(random.choice(OUTCOMES))
+
+
+Yao.NAMES = {y.value: y.name.replace('_', ' ').title() for y in Yao}
+
+
 
